1461bff0450653eb5739c927462f542c47183816
VyacheslavAtamanyuk <vyacheslav.atamanyuk@mail.ru>
2023-12-05 22:45:02 +0300

    v2.0

--- lib/labwork_class.cpp
+++ lib/labwork_class.cpp
@@ -1,6 +1,11 @@
 #include "labwork_class.h"
 
-SubSubArr& SubSubArr::operator=(size_t new_num) {
+//Конструктор и методы Element (перегрузка ввода-вывода в Array3D
+
+Element::Element(size_t new_ind_of_sub_sub_arr, size_t new_ind_of_sub_arr, size_t new_ind_of_arr, Array3D* new_parent_for_sub_sub_arr):
+    ind_of_sub_sub_arr(new_ind_of_sub_sub_arr), ind_of_sub_arr(new_ind_of_sub_arr), ind_of_arr(new_ind_of_arr), parent_for_sub_sub_arr(new_parent_for_sub_sub_arr) {};
+
+Element& Element::operator=(size_t new_num) {
     size_t location = ind_of_arr * parent_for_sub_sub_arr->y * parent_for_sub_sub_arr->z +
             ind_of_sub_arr*parent_for_sub_sub_arr->z + ind_of_sub_sub_arr;
     parent_for_sub_sub_arr->arr[location] = new_num & 0xFFFF;
@@ -8,35 +13,50 @@ SubSubArr& SubSubArr::operator=(size_t new_num) {
     return *this;
 }
 
-SubSubArr& SubArr::operator[](size_t ind) {
-    sub_sub_array.ind_of_sub_sub_arr = ind;
-    sub_sub_array.ind_of_sub_arr = ind_of_sub_arr;
-    sub_sub_array.ind_of_arr = ind_of_arr;
-    sub_sub_array.parent_for_sub_sub_arr = parent_for_sub_arr;
-    return sub_sub_array;
+//Конструктор и методы Array (перегрузка ввода-вывода в Array3D)
+
+Array::Array(size_t new_ind_of_sub_arr, size_t new_ind_of_arr, Array3D *new_parent_for_sub_arr): ind_of_sub_arr(new_ind_of_sub_arr),
+    ind_of_arr(new_ind_of_arr), parent_for_sub_arr(new_parent_for_sub_arr) {};
+
+Element Array::operator[](size_t ind) {
+    return {ind, ind_of_sub_arr, ind_of_arr, parent_for_sub_arr};
 }
 
-SubArr& Arr::operator[](size_t ind) {
-    sub_array.ind_of_sub_arr = ind;
-    sub_array.ind_of_arr = ind_of_arr;
-    sub_array.parent_for_sub_arr = parent_for_arr;
-    return sub_array;
+//Конструктор и методы Array2D (перегрузка ввода-вывода в Array3D)
+
+Array2D::Array2D(size_t new_ind_of_arr, Array3D *new_parent_for_arr): ind_of_arr(new_ind_of_arr), parent_for_arr(new_parent_for_arr) {};
+
+Array Array2D::operator[](size_t ind) {
+    return {ind, ind_of_arr, parent_for_arr};
 }
 
-Arr& SeventeenBits::operator[](size_t ind) {
-    array.ind_of_arr = ind;
-    array.parent_for_arr = this;
-    return array;
+//Конструктор, деструктор, констукторы копирования и присваивания, методы Array3D
+
+Array3D Array3D::make_array(size_t size1, size_t size2, size_t size3) {
+    Array3D new_class;
+    new_class.arr = new uint16_t[size1 * size2 * size3];
+    new_class.one_bit = new uint8_t[(size1 * size2 * size3) / 8 + 1]{};
+    new_class.x = size1;
+    new_class.y = size2;
+    new_class.z = size3;
+    return new_class;
 }
 
-SeventeenBits::SeventeenBits() {};
+Array3D::Array3D() {};
 
-SeventeenBits::~SeventeenBits() {
+Array3D::Array3D(size_t new_x, size_t new_y, size_t new_z): x(new_x), y(new_y), z(new_z) {
+    uint16_t* new_arr = new uint16_t [new_x * new_y * new_z];
+    uint8_t* new_one_bit = new uint8_t[(new_x * new_y * new_z) / 8 + 1];
+    arr = new_arr;
+    one_bit = new_one_bit;
+}
+
+Array3D::~Array3D() {
     delete[] arr;
     delete[] one_bit;
 }
 
-SeventeenBits::SeventeenBits(const SeventeenBits& other) {
+Array3D::Array3D(const Array3D& other) {
     x = other.x;
     y = other.y;
     z = other.z;
@@ -48,7 +68,7 @@ SeventeenBits::SeventeenBits(const SeventeenBits& other) {
     }
 }
 
-SeventeenBits& SeventeenBits::operator=(const SeventeenBits& other) {
+Array3D& Array3D::operator=(const Array3D& other) {
     if (this != &other) {
         uint16_t* new_arr = new uint16_t[other.x * other.y * other.z];
         uint8_t* new_one_bit = new uint8_t[(other.x * other.y * other.z) / 8 + 1];
@@ -69,23 +89,12 @@ SeventeenBits& SeventeenBits::operator=(const SeventeenBits& other) {
     return *this;
 }
 
-SeventeenBits SeventeenBits::make_array(size_t size1, size_t size2, size_t size3) {
-    SeventeenBits new_class;
-    new_class.arr = new uint16_t[size1 * size2 * size3];
-    new_class.one_bit = new uint8_t[(size1 * size2 * size3) / 8 + 1]{};
-    new_class.x = size1;
-    new_class.y = size2;
-    new_class.z = size3;
-    return new_class;
+Array2D Array3D::operator[](size_t ind) {
+    return {ind, this};
 }
 
-SeventeenBits SeventeenBits::operator+(const SeventeenBits& rhs) {
-    SeventeenBits new_class;
-    new_class.arr = new uint16_t[x * y * z];
-    new_class.one_bit = new uint8_t[x * y * z / 8 + 1]{};
-    new_class.x = x;
-    new_class.y = y;
-    new_class.z = z;
+Array3D Array3D::operator+(const Array3D& rhs) {
+    Array3D new_class(x, y, z);
     for (int i = 0; i < x; ++i) {
         for (int j = 0; j < y; ++j) {
             for (int k = 0; k < z; ++k) {
@@ -100,17 +109,12 @@ SeventeenBits SeventeenBits::operator+(const SeventeenBits& rhs) {
     return new_class;
 }
 
-SeventeenBits SeventeenBits::operator-(const SeventeenBits& rhs) {
-    SeventeenBits new_class;
-    new_class.arr = new uint16_t[x * y * z];
-    new_class.one_bit = new uint8_t[x * y * z / 8 + 1]{};
-    new_class.x = x;
-    new_class.y = y;
-    new_class.z = z;
+Array3D Array3D::operator-(const Array3D& rhs) {
+    Array3D new_class(x, y, z);
     for (int i = 0; i < x; ++i) {
         for (int j = 0; j < y; ++j) {
             for (int k = 0; k < z; ++k) {
-                size_t location = i * y * z + j*z + k;
+                size_t location = i * y * z + j * z + k;
                 uint32_t left = ((((one_bit[location / 8] >> location%8) & 1) << 16) + arr[location]);
                 uint32_t right = ((((rhs.one_bit[location / 8] >> location%8) & 1) << 16) + rhs.arr[location]);
                 if (left > right) {
@@ -126,13 +130,8 @@ SeventeenBits SeventeenBits::operator-(const SeventeenBits& rhs) {
 }
 
 //int, т.к хочу обработать случаи, когда на вход подается число <= 0, а size_t - unsigned
-SeventeenBits SeventeenBits::operator*(int num) {
-    SeventeenBits new_class;
-    new_class.arr = new uint16_t[x * y * z];
-    new_class.one_bit = new uint8_t[x * y * z / 8 + 1]{};
-    new_class.x = x;
-    new_class.y = y;
-    new_class.z = z;
+Array3D Array3D::operator*(int num) {
+    Array3D new_class(x, y, z);
     for (int i = 0; i < x; ++i) {
         for (int j = 0; j < y; ++j) {
             for (int k = 0; k < z; ++k) {
@@ -150,7 +149,25 @@ SeventeenBits SeventeenBits::operator*(int num) {
     return new_class;
 }
 
-std::ostream& operator<<(std::ostream& output, const SubSubArr& data) {
+std::istream& operator>>(std::istream& input, Array3D& data) {
+    size_t new_x, new_y, new_z;
+    input >> new_x >> new_y >> new_z;
+    Array3D new_data(new_x, new_y, new_z);
+    data = new_data;
+    for (int i = 0; i < new_x; ++i) {
+        for (int j = 0; j < new_y; ++j) {
+            for (int k = 0; k < new_z; ++k) {
+                uint32_t num;
+                input >> num;
+                data.arr[i * new_y * new_z + j * new_z + k] = num & 0xFFFF;
+                data.one_bit[(i * new_y * new_z + j * new_z + k) / 8] |= ((num >> 16) & 1) << ((i * new_y * new_z + j * new_z + k) % 8);
+            }
+        }
+    }
+    return input;
+}
+
+std::ostream& operator<<(std::ostream& output, const Element& data) {
     size_t location = data.ind_of_arr * data.parent_for_sub_sub_arr->y * data.parent_for_sub_sub_arr->z +
             data.ind_of_sub_arr * data.parent_for_sub_sub_arr->z + data.ind_of_sub_sub_arr;
     output << (data.parent_for_sub_sub_arr->arr[location] +
@@ -158,7 +175,7 @@ std::ostream& operator<<(std::ostream& output, const SubSubArr& data) {
     return output;
 }
 
-std::ostream& operator<<(std::ostream& output, const SubArr& data) {
+std::ostream& operator<<(std::ostream& output, const Array& data) {
     size_t location = data.ind_of_arr * data.parent_for_sub_arr->y * data.parent_for_sub_arr->z +
                       data.ind_of_sub_arr * data.parent_for_sub_arr->z;
     output << '[';
@@ -174,13 +191,13 @@ std::ostream& operator<<(std::ostream& output, const SubArr& data) {
     return output;
 }
 
-std::ostream& operator<<(std::ostream& output, const Arr& data) {
+std::ostream& operator<<(std::ostream& output, const Array2D& data) {
     size_t location = data.ind_of_arr * data.parent_for_arr->y * data.parent_for_arr->z;
     output << '[';
     for (size_t i = 0; i < data.parent_for_arr->y; ++i) {
         output << '[';
         for (size_t j = 0; j < data.parent_for_arr->z; ++j) {
-            output << (data.parent_for_arr->arr[location + i*data.parent_for_arr->z + j] +
+            output << (data.parent_for_arr->arr[location + i * data.parent_for_arr->z + j] +
             (((data.parent_for_arr->one_bit[(location + i) / 8] >> (location + 1 % 8)) & 1) << 16));
             if (j == data.parent_for_arr->z - 1) {
                 output << ']';
@@ -197,7 +214,7 @@ std::ostream& operator<<(std::ostream& output, const Arr& data) {
     return output;
 }
 
-std::ostream& operator<<(std::ostream& output, const SeventeenBits& data) {
+std::ostream& operator<<(std::ostream& output, const Array3D& data) {
     output << '[';
     for (int i = 0; i < data.x; ++i) {
         output << '[';
@@ -227,30 +244,7 @@ std::ostream& operator<<(std::ostream& output, const SeventeenBits& data) {
     return output;
 }
 
-std::istream& operator>>(std::istream& input, SeventeenBits& data) {
-    size_t new_x;
-    size_t new_y;
-    size_t new_z;
-    input >> new_x >> new_y >> new_z;
-    data.x = new_x;
-    data.y = new_y;
-    data.z = new_z;
-    data.arr = new uint16_t[new_x * new_y * new_z];
-    data.one_bit = new uint8_t[(new_x * new_y * new_z) / 8 + 1];
-    uint32_t num;
-    for (int i = 0; i < new_x; ++i) {
-        for (int j = 0; j < new_y; ++j) {
-            for (int k = 0; k < new_z; ++k) {
-                input >> num;
-                data.arr[i * new_y * new_z + j * new_z + k] = num & 0xFFFF;
-                data.one_bit[(i * new_y * new_z + j * new_z + k) / 8] |= ((num >> 16) & 1) << ((i * new_y * new_z + j * new_z + k) % 8);
-            }
-        }
-    }
-    return input;
-}
-
-bool operator==(const SubSubArr& object, size_t num) {
+bool operator==(const Element& object, size_t num) {
     size_t location = object.ind_of_arr * object.parent_for_sub_sub_arr->y * object.parent_for_sub_sub_arr->z +
                       object.ind_of_sub_arr * object.parent_for_sub_sub_arr->z + object.ind_of_sub_sub_arr;
     return object.parent_for_sub_sub_arr->arr[location] + (((object.parent_for_sub_sub_arr->one_bit[location / 8] >> (location % 8)) & 1) << 16) == num;
--- lib/labwork_class.h
+++ lib/labwork_class.h
@@ -4,86 +4,87 @@
 #include <iostream>
 #include <cstdint>
 
-class SeventeenBits;
+class Array3D;
 
-class SubSubArr {
+class Element {
+public:
+    Element(size_t new_ind_of_sub_sub_arr, size_t new_ind_of_sub_arr, size_t new_ind_of_arr, Array3D* new_parent_for_sub_sub_arr);
+
+    Element& operator=(size_t new_num);
+
+    friend std::ostream& operator<<(std::ostream& output, const Element& data);
+
+    friend bool operator==(const Element& object, size_t num);
+
+    friend class Array;
 private:
     size_t ind_of_sub_sub_arr;
     size_t ind_of_sub_arr;
     size_t ind_of_arr;
-    SeventeenBits* parent_for_sub_sub_arr;
-public:
-    SubSubArr& operator=(size_t new_num);
-
-    friend std::ostream& operator<<(std::ostream& output, const SubSubArr& data);
-
-    friend bool operator==(const SubSubArr& object, size_t num);
-
-    friend class SubArr;
+    Array3D* parent_for_sub_sub_arr;
 };
 
-class SubArr {
+class Array {
+public:
+    Array(size_t new_ind_of_sub_arr, size_t new_ind_of_arr, Array3D* new_parent_for_sub_arr);
+
+    Element operator[](size_t ind);
+
+    friend std::ostream& operator<<(std::ostream& output, const Array& data);
+
+    friend class Array2D;
 private:
-    SubSubArr sub_sub_array;
     size_t ind_of_sub_arr;
     size_t ind_of_arr;
-    SeventeenBits* parent_for_sub_arr;
-public:
-    SubSubArr& operator[](size_t ind);
-
-    friend std::ostream& operator<<(std::ostream& output, const SubArr& data);
-
-    friend class Arr;
+    Array3D* parent_for_sub_arr;
 };
 
-class Arr {
+class Array2D {
+public:
+    Array2D(size_t new_ind_of_arr, Array3D* new_parent_for_arr);
+
+    Array operator[](size_t ind);
+
+    friend std::ostream& operator<<(std::ostream& output, const Array2D& data);
+
+    friend class Array3D;
 private:
-    SubArr sub_array;
     size_t ind_of_arr;
-    SeventeenBits* parent_for_arr;
-public:
-    SubArr& operator[](size_t ind);
-
-    friend std::ostream& operator<<(std::ostream& output, const Arr& data);
-
-    friend class SeventeenBits;
+    Array3D* parent_for_arr;
 };
 
-class SeventeenBits {
+class Array3D {
+public:
+    static Array3D make_array(size_t size1, size_t size2, size_t size3);
+
+    Array3D();
+    Array3D(size_t new_x, size_t new_y, size_t new_z);
+    ~Array3D();
+    Array3D(const Array3D& other);
+    Array3D& operator=(const Array3D& other);
+
+    Array2D operator[](size_t ind);
+
+    Array3D operator+(const Array3D& rhs);
+    Array3D operator-(const Array3D& rhs);
+    Array3D operator*(int num);
+
+    friend std::istream& operator>>(std::istream& data, Array3D& input);
+
+    friend std::ostream& operator<<(std::ostream& output, const Element& data);
+    friend std::ostream& operator<<(std::ostream& output, const Array& data);
+    friend std::ostream& operator<<(std::ostream& output, const Array2D& data);
+    friend std::ostream& operator<<(std::ostream& output, const Array3D& data);
+
+    friend bool operator==(const Element& object, size_t num);
+
+    friend class Element;
 private:
-    Arr array;
     size_t x;
     size_t y;
     size_t z;
     uint16_t* arr;
     uint8_t* one_bit;
-public:
-    static SeventeenBits make_array(size_t size1, size_t size2, size_t size3);
-
-    SeventeenBits();
-
-    ~SeventeenBits();
-
-    SeventeenBits(const SeventeenBits& other);
-
-    SeventeenBits& operator=(const SeventeenBits& other);
-
-    Arr& operator[](size_t ind);
-
-    SeventeenBits operator+(const SeventeenBits& rhs);
-    SeventeenBits operator-(const SeventeenBits& rhs);
-    SeventeenBits operator*(int num);
-
-    friend std::istream& operator>>(std::istream& data, SeventeenBits& input);
-
-    friend std::ostream& operator<<(std::ostream& output, const SubSubArr& data);
-    friend std::ostream& operator<<(std::ostream& output, const SubArr& data);
-    friend std::ostream& operator<<(std::ostream& output, const Arr& data);
-    friend std::ostream& operator<<(std::ostream& output, const SeventeenBits& data);
-
-    friend bool operator==(const SubSubArr& object, size_t num);
-
-    friend class SubSubArr;
 };
 
 #endif //LAB5_LABWORK_CLASS_H
